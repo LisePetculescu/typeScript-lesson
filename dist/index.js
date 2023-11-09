69ae6e617f9a19d96f3507040b9e2e892893083e
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-11-09 13:00:42 +0100

    gg

--- dist/index.js
+++ dist/index.js
@@ -6,14 +6,13 @@ let level;
 level = "one";
 level = 1;
 function calculateTax(income) {
-    if (income > 500000) {
-        let tax = income * 0.4;
-        return tax;
-    }
+  if (income > 500000) {
+    let tax = income * 0.4;
+    return tax;
+  }
 }
 let tax = calculateTax(600000);
 console.log(tax);
-
 
 let q = 100;
 //# sourceMappingURL=index.js.map
